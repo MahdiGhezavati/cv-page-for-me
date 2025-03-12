538508eef22006c2de24fdc184935e910c41082d
MahdiGhezavati <gorganroyal@gmail.com>
2025-03-12 03:52:41 +0330

    add static and media root and urls

--- MYcv/urls.py
+++ MYcv/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path , include
 from info.views import *
 from contact.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,3 +28,6 @@ urlpatterns = [
     path("" ,include("info.urls")) ,
     path("contact" ,include("contact.urls")) ,
 ]
+
+urlpatterns += static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
