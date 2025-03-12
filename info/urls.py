d310ecf1c75e507e09a6feac2db00d7a3bfd92fa
MahdiGhezavati <gorganroyal@gmail.com>
2025-03-12 03:51:12 +0330

    edit the name of defined
    in views and urls

--- info/urls.py
+++ info/urls.py
@@ -3,5 +3,5 @@ from info.views import *
 
 app_name = "information"
 urlpatterns = [
-    path("",info , name="information"),
+    path("", info_page , name="information"),
 ]
--- info/views.py
+++ info/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def info(request):
+def info_page(request):
     return render(request , "information/info.html")
